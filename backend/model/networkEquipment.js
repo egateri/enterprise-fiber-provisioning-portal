be1353c44fd71149bfa53450f44b0773b352d90c
kipgeoffry <kipgeoffry@gmail.com>
2023-11-09 10:03:28 +0300

    updated model names for provideredgerouter and networkequipment

---
+++ backend/model/networkEquipment.js
@@ -0,0 +1,59 @@
+
+const mongoose = require("mongoose");
+
+const neSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    ip:{
+        type:String, 
+        require:true
+    },
+    model:{
+        type:String,
+        require:true
+    },
+    aggregatingPe:{
+        type:String,
+        require:true
+    },
+    peIp:{
+        peIp1:{
+            type:String,
+            require:true
+        },
+        peIp2:{
+            type:String,
+        }
+    },   
+    carrierVlan:{
+        internet: {
+            type: Number,
+            required: true
+        },
+        mpls: {
+            type: Number,
+            required: true
+        }
+    },
+    peInterface:{
+        type:String,
+        require:true
+    },
+    deviceType:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:mongoose.Schema.Types.Date,
+        required:true,
+        default:Date.now()
+    }
+
+})
+module.exports = mongoose.model('NetworkEquipment', neSchema)
+
+
+
+
---
+++ backend/model/providerEdgeRouters.js
@@ -0,0 +1,46 @@
+
+const mongoose = require("mongoose");
+
+const peSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    model:{
+        type:String,
+        require:true
+    },
+    peIp1:{
+        type:String,
+        require:true
+    },
+    peIp2:{
+        type:String,
+        require:true
+    },
+    peInterface1:{
+        type:String,
+        require:true
+    },
+    peInterface2:{
+        type:String,
+    },
+    peInterface3:{
+        type:String,
+    },
+    deviceType:{
+        type:String,
+        required:true
+    },
+    mcLag:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    createdAt:{
+        type:mongoose.Schema.Types.Date,
+        required:true,
+        default:Date.now()
+    }
+})
+module.exports = mongoose.model('PERouter', peSchema)
