30ecdecc8f1a82b138ebdd10bedee0b3906ce4c5
Eliud Gateri <egateri@gmail.com>
2023-11-10 11:58:55 +0300

    update the index route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,19 @@ const PERouter =require("../model/providerEdgeRouter")
 const bcrypt=require("bcrypt");
 const jwt= require("jsonwebtoken");
 const express =require("express");
+const {v4 : uuidv4} = require('uuid');
 const router =express.Router();
 
 router.get("/",(req,res)=>{
-    res.status(200).json({message:"Welcome to Fiber Provisioning portal"});
+    const timestamp =Date.now();
+    const requestId = uuidv4();
+    res.status(200).json({
+        header:{
+            requestId:requestId,
+            status:200, 
+             message:"OK",
+             timestamp:timestamp
+            },
+            body:{message:"Welcome to Fiber Provisioning portal API"}});
 });
 module.exports =router;
