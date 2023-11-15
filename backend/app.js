9e02f9aff12f6f60ff70a1be3c3bb1800b3af1fe
kipgeoffry <kipgeoffry@gmail.com>
2023-11-15 08:50:33 +0300

    added pe and ne routes and router

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,13 @@ const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
 const updateRouter = require('./routes/update');
 const deleteRouter = require('./routes/delete');
+const peRouter = require('./routes/peRoutes')
+const neRouter = require('./routes/neRoutes');
 const express = require('express');
 const PORT = process.env.PORT||5500;
 const app = express();
+
+
 app.set(db);
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +24,8 @@ app.use("/v1/users",registerRouter);
 app.use("/v1/users",loginRouter);
 app.use("/v1/users",updateRouter);
 app.use("/v1/users",deleteRouter);
+app.use("/v1/pes",peRouter);
+app.use("/v1/nes",neRouter);
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 })
