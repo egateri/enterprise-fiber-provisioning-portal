a7ef148a8c35c923b907198d09a0b2a346e43d12
Eliud Gateri <egateri@gmail.com>
2023-12-08 15:12:48 +0300

    Formatting the code

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,64 +1,67 @@
 const Header = () => {
   const email = localStorage.getItem("email");
-  const handleClick = ()=>{
+  const handleClick = () => {
     localStorage.clear();
     window.location.reload();
-    
-  }
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-          <a className="navbar-brand" href="/">
-            Home
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <a className="navbar-brand" href="/">
+          Home
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
             {/* <li className="nav-item">
                 <a className="nav-link" href="/register">
                   Register
                 </a>
               </li> */}
-              {/* <li className="nav-item">
+            {/* <li className="nav-item">
                 <a className="nav-link" href="/">
                   Login
                 </a>
               </li> */}
-              <li className="nav-item">
-                <a className="nav-link" href="/" >
-                  PE ROUTER
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/" >
-                  NE ROUTER
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  {email}
-                </a>
-              </li>
-              <li className="nav-item">
-                <button type="button" class="btn btn-outline-danger" onClick ={handleClick}>
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  };
-  
-  export default Header;
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                PE ROUTER
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                NE ROUTER
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                {email}
+              </a>
+            </li>
+            <li className="nav-item">
+              <button
+                type="button"
+                class="btn btn-outline-danger"
+                onClick={handleClick}
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
